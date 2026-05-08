c00c65c279804186496c758a88c96a85e23c4ca7
Luiza Scolar <scolar441@gmail.com>
2026-05-08 10:29:39 -0300

    Primeiro commit da aula do dia 08/05

---
+++ ProjetoNiveisLogin/Models/Colaborador.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoNiveisLogin.Models
+{
+    public class Colaborador
+    {
+        [Display(Name = "Código", Description = "Código.")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome completo", Description = "Nome e Sobrenome.")]
+        [Required(ErrorMessage = "O nome completo é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Celular")]
+        [Required(ErrorMessage = "O celular é obrigatório.")]
+        public string Telefone { get; set; }
+
+        [Display(Name = "CPF")]
+        [Required(ErrorMessage = "O CPF é obrigatório.")]
+        public string CPF { get; set; }
+
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "O email não é válido.")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Informe um email válido.")]
+        public string Email { get; set; }
+
+        [Display(Name = "Senha")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        [StringLength(10, MinimumLength = 6, ErrorMessage = "A senha deve ter entre 6 e 10 caracteres.")]
+        public string Senha { get; set; }
+
+        [Display(Name = "Tipo")]
+        [Required(ErrorMessage = "O tipo é obrigatório.")]
+        public string Tipo { get; set; }
+    }
+}

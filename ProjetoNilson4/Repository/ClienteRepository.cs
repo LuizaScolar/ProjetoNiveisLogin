31f2fa8751f19ede02d9afbb52f183334ede1874
nicolasSchinagel <drivepoweringames@gmail.com>
2026-05-12 09:25:22 -0300

    adicionando repositoria e alguns métodos

---
+++ ProjetoNilson4/Repository/ClienteRepository.cs
@@ -0,0 +1,107 @@
+﻿using MySql.Data.MySqlClient;
+using ProjetoNilson4.Models;
+using ProjetoNilson4.Repository.Contract;
+using System.Data;
+using X.PagedList;
+
+namespace ProjetoNilson4.Repository
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly string _conexaoMySQL;
+        IConfiguration _config;
+        public ClienteRepository(IConfiguration conf)
+        {
+            _conexaoMySQL = conf.GetConnectionString("ConexaoMySQL");
+            _config = conf;
+        }
+        public Cliente Login(string Email, string Senha)
+        {
+            using(var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from cliente where Email = @Email and Senha = @Senha", conexao);
+
+                cmd.Parameters.Add("@Email", MySqlDbType.VarChar).Value = Email;
+                cmd.Parameters.Add("@Senha", MySqlDbType.VarChar).Value = Senha;
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                MySqlDataReader dr;
+
+                Cliente cliente = new Cliente();
+                dr = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+
+                while(dr.Read())
+                {
+                    cliente.Id = Convert.ToInt32(dr["Id"]);
+                    cliente.Nome = Convert.ToString(dr["Nome"]);
+                    cliente.Nascimento = Convert.ToDateTime(dr["Nascimento"]);
+
+                    cliente.Sexo = Convert.ToString(dr["Sexo"]);
+                    cliente.CPF = Convert.ToString(dr["CPF"]);
+                    cliente.Telefone = Convert.ToString(dr["Telefone"]);
+                    cliente.Situacao = Convert.ToString(dr["Situacao"]);
+
+                    cliente.Email = Convert.ToString(dr["Email"]);
+                    cliente.Senha = Convert.ToString(dr["Senha"]);
+                }
+                return cliente;
+            }
+        }
+
+        public void Atualizar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public Cliente ObterCliente(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Cliente> ObterTodosClientes()
+        {
+            List<Cliente> cliList = new List<Cliente>();
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand(" select * from Cliente", conexao);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+
+                conexao.Close();
+
+                foreach(DataRow dr in dt.Rows)
+                {
+                    cliList.Add(
+                        new Cliente
+                        {
+                            // paramos aqui
+                        }
+                        );
+                }
+
+            }
+        }
+
+        public IPagedList<Cliente> ObterTodosClientes(int? pagina, string pesquisa)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

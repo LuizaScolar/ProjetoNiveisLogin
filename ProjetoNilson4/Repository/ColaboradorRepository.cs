c2e2e06a05915e148716143536ff81a8ce4ce8ca
nicolasSchinagel <drivepoweringames@gmail.com>
2026-05-26 09:16:18 -0300

    colocando métodos de colaborador

--- ProjetoNilson4/Repository/ColaboradorRepository.cs
+++ ProjetoNilson4/Repository/ColaboradorRepository.cs
@@ -4,19 +4,38 @@ using ProjetoNilson4.Models.Constant;
 using ProjetoNilson4.Repository.Contract;
 using System.Data;
 using X.PagedList;
+using X.PagedList.Extensions;
+using static Org.BouncyCastle.Math.EC.ECCurve;
 namespace ProjetoNilson4.Repository
 {
     public class ColaboradorRepository : IColaboradorRepository
     {
         private readonly string _conexaoMySQL;
+        private IConfiguration _conf;
 
         public ColaboradorRepository(IConfiguration conf)
         {
             _conexaoMySQL = conf.GetConnectionString("ConexaoMySQL");
+            _conf = conf;
         }
         public void Atualizar(Colaborador colaborador)
         {
-            throw new NotImplementedException();
+            string Tipo = ColaboradorTipoConstant.Comum;
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("update Colaborador set Nome=@Nome, Email=@Email, Senha=@Senha, Tipo=@Tipo where Id=@Id", conexao);
+
+                cmd.Parameters.Add("@Nome", MySqlDbType.VarChar).Value = colaborador.Nome;
+                cmd.Parameters.Add("@CPF", MySqlDbType.VarChar).Value = colaborador.CPF;
+                cmd.Parameters.Add("@Telefone", MySqlDbType.VarChar).Value = colaborador.Telefone;
+                cmd.Parameters.Add("@Email", MySqlDbType.VarChar).Value = colaborador.Email;
+                cmd.Parameters.Add("@Senha", MySqlDbType.VarChar).Value = colaborador.Senha;
+                cmd.Parameters.Add("@Tipo", MySqlDbType.VarChar).Value = Tipo;
+
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
         }
 
         public void AtualizarSenha(Colaborador colaborador)
@@ -26,9 +45,9 @@ namespace ProjetoNilson4.Repository
 
         public void Cadastrar(Colaborador colaborador)
         {
-            string Comum = ColaboradorTipoConstant.Comum;
+            string Tipo = ColaboradorTipoConstant.Comum;
 
-        using(var conexao = new MySqlConnection(_conexaoMySQL))
+           using(var conexao = new MySqlConnection(_conexaoMySQL))
             {
                 conexao.Open();
 
@@ -40,7 +59,7 @@ namespace ProjetoNilson4.Repository
                 cmd.Parameters.Add("@Telefone", MySqlDbType.VarChar).Value = colaborador.Telefone;
                 cmd.Parameters.Add("@Email", MySqlDbType.VarChar).Value = colaborador.Email;
                 cmd.Parameters.Add("@Senha", MySqlDbType.VarChar).Value = colaborador.Senha;
-                cmd.Parameters.Add("@Tipo", MySqlDbType.VarChar).Value = Comum;
+                cmd.Parameters.Add("@Tipo", MySqlDbType.VarChar).Value = Tipo;
 
                 cmd.ExecuteNonQuery();
                 conexao.Close();
@@ -82,22 +101,108 @@ namespace ProjetoNilson4.Repository
 
         public Colaborador ObterColaborador(int Id)
         {
-            throw new NotImplementedException();
-        }
+            using(var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("Select * from Colaborador where Id=@Id", conexao);
+                cmd.Parameters.AddWithValue("@Id", Id);
 
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                MySqlDataReader dr;
+
+                Colaborador colaborador = new Colaborador();
+                dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                while (dr.Read())
+                {
+                    colaborador.Id = (Int32)(dr["Id"]);
+                    colaborador.Nome = (string)(dr["Nome"]);
+                    colaborador.Email = (string)(dr["Email"]);
+                    colaborador.Senha = (string)(dr["Senha"]);
+                    colaborador.Tipo = (string)(dr["Tipo"]);
+                }
+                return colaborador;
+            }
+        }
+       
+        // TERMINAR DEPOIS
         public List<Colaborador> ObterColaboradorPorEmail(string email)
         {
-            throw new NotImplementedException();
+            List<Colaborador> colabList = new List<Colaborador>();
+            using(var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("Select * from Colaborador where Email=@Email", conexao);
+                cmd.Parameters.AddWithValue("@Email", email);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                
+                }
+                
+            }
         }
 
+        
         public IEnumerable<Colaborador> ObterTodosColaboradores()
         {
-            throw new NotImplementedException();
+            List<Colaborador> colabList = new List<Colaborador>();
+            using(var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("Select * from Colaborador", conexao);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                conexao.Close();
+
+                foreach(DataRow dr in dt.Rows)
+                {
+                    colabList.Add(
+                        new Colaborador
+                        {
+                            Id = Convert.ToInt32(dr["Id"]),
+                            Nome = (string)(dr["Nome"]),
+                            Email = (string)(dr["Email"]),
+                            Senha = (string)(dr["Senha"]),
+                            Tipo = (string)(dr["Tipo"]),
+                        });
+                }
+                return colabList;
+            }
         }
 
         public IPagedList<Colaborador> ObterTodosColaboradores(int? pagina)
         {
-            throw new NotImplementedException();
+            int RegistroPorPagina = _conf.GetValue<int>("RegistroPorPagina");
+
+            int NumeroPagina = pagina ?? 1;
+            List<Colaborador> ListCat = new List<Colaborador>();
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from colaborador;", conexao);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+                conexao.Close();
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    ListCat.Add(
+                        new Colaborador
+                        {
+                            Id = Convert.ToInt32(dr["Id"]),
+                            Nome = (string)(dr["Nome"]),
+                            Senha = (string)(dr["Senha"]),
+                            Email = (string)(dr["Email"]),
+                            Tipo = (string)(dr["Senha"])
+
+                        });
+                }
+                return ListCat.ToPagedList<Colaborador>(NumeroPagina, RegistroPorPagina);
+            }
         }
     }
 }
--- ProjetoNilson4/obj/Debug/net10.0/ProjetoNilson4.AssemblyInfo.cs
+++ ProjetoNilson4/obj/Debug/net10.0/ProjetoNilson4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoNilson4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d922c1462f5abb150dc995cbbbb44b4f8adb18fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fbc14f84404ae66b6724971154cc3a038b356aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoNilson4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoNilson4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

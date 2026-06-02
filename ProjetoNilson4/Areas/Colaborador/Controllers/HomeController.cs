e3540f7f78dfe4834c2360fecd7fd5159b6e8fba
Thiago <thiagolimeira732@gmail.com>
2026-06-02 08:29:27 -0300

    arrumando o codigo

---
+++ ProjetoNilson4/Areas/Colaborador/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoNilson4.Libraries.Login;
+using ProjetoNilson4.Repository.Contract;
+
+namespace ProjetoNilson4.Areas.Colaborador.Controllers
+{
+    [Area("Colaborador")]
+    public class HomeController : Controller
+    {
+        private IColaboradorRepository _repositoryColaborador;
+        private LoginColaborador _loginColaborador;
+
+        public HomeController(IColaboradorRepository repositoryColaborador, LoginColaborador loginColaborador)
+        {
+            _repositoryColaborador = repositoryColaborador;
+            _loginColaborador = loginColaborador;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

83efb3621595388033c077a99e8663bcc6b124bf
Samuel <sam1219barbosa@gmail.com>
2026-06-19 10:31:36 -0300

    commit da aula do dia 19/06

--- ProjetoNilson4/obj/Debug/net10.0/ProjetoNilson4.AssemblyInfo.cs
+++ ProjetoNilson4/obj/Debug/net10.0/ProjetoNilson4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoNilson4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28a84f4aa6f695f21c98f9c6e9a28e225d7fcefd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7137361d0e71ccda4942d7b59a5aa936f920a33")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoNilson4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoNilson4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

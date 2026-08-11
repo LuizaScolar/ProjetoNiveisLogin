90718e216beb8e004fbe730ccd5e9d21198f9d09
LuizaScolar <scolar441@gmail.com>
2026-08-11 08:46:57 -0300

    atualizando a parte visual do codigo

--- ProjetoNilson4/obj/Debug/net10.0/ProjetoNilson4.AssemblyInfo.cs
+++ ProjetoNilson4/obj/Debug/net10.0/ProjetoNilson4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoNilson4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7137361d0e71ccda4942d7b59a5aa936f920a33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83efb3621595388033c077a99e8663bcc6b124bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoNilson4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoNilson4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
